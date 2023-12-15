0af1455dded7aaf7d26129bee5ce3e87c6fc5f3b
girish <girish.mishra1@gmail.com>
2023-12-15 23:08:19 +0530

    commit it

---
+++ src/com/examples/BitwiseShiftOperation.java
@@ -0,0 +1,12 @@
+package src.com.examples;
+
+public class BitwiseShiftOperation {
+    public static void main(String[] args) {
+        int i = 5;
+        int j = 8;
+        System.out.println(2<<j);
+        System.out.println(j<<2);
+        System.out.println(i>>2);
+        System.out.println(j>>2);
+    }
+}
